5c01b4b9cb4a42000374e75855e136dec7cad032
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-02-23 08:20:48 +0530

    save all commands in powershell history

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -3,3 +3,5 @@ $env:LANG = 'en_US.UTF-8'
 function restic {
   & restic.exe --password-command 'bw get password "restic / tarun / my-files"' @args
 }
+
+Set-PSReadLineOption -HistorySaveStyle SaveEverything
