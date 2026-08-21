b22a37fb9ef2748e056373f90d71c1c45e6caab7
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-08-21 17:29:15 +0000

    integrate llama into homelab compose config

--- llama/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-podman run --rm -d -p 8080:8080 --gpus all -e HF_HOME=/models -v /srv/data/llama/models:/models ghcr.io/ggml-org/llama.cpp:server-cuda -hf mistralai/Ministral-3-3B-Instruct-2512-GGUF:Q4_K_M -c 16384 -ngl 18
